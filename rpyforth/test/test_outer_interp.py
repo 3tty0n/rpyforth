b3bd7622cf50da64660440d8da500af3af812b33
kota0709 <225haka0011@gmail.com>
2025-10-23 11:43:59 +0900

    add test ROT MAX MIN ABS NEGATE DROP2 SWAP2 OVER2

--- rpyforth/test/test_outer_interp.py
+++ rpyforth/test/test_outer_interp.py
@@ -1,4 +1,3 @@
-from rpyforth.objects import W_StringObject
 from rpyforth.outer_interp import OuterInterpreter
 from rpyforth.inner_interp import InnerInterpreter
 
@@ -8,22 +7,6 @@ def run(line):
     outer.interpret_line(line)
     return inner
 
-def run_and_pop(line):
-    return run(line).pop_ds()
-
-def test_basic_primitives():
-    assert run_and_pop(": SQUARE DUP * ; 3 SQUARE").intval == 9
-    assert run_and_pop(": INC 1 + ;  5 INC").intval == 6
-
-def test_STORE_FETCH():
-    assert run_and_pop("5 0 !    0 @").intval == 5
-    assert run_and_pop("VARIABLE X    123 X !    X @").intval == 123
-    assert run_and_pop("VARIABLE A    10 A !    A @ 5 + A !    A @").intval == 15
-    assert run_and_pop(""": SQUARE DUP * ;    VARIABLE N
-7 N !    N @ SQUARE""").intval == 49
-
-
-def test_PNO():
-    assert run_and_pop("DECIMAL  12345 <# #S #>").strval == '12345'
-    assert run_and_pop("HEX      255   <# #S #>").strval == 'FF'
-    assert run_and_pop("BINARY   5     <# #S #>").strval == '101'
+def test_interp_line():
+    assert run(": SQUARE DUP * ; 3 SQUARE").ds.pop().intval == 9
+    assert run(": INC 1 + ;  5 INC").ds.pop().intval == 6
