c3cf6e3d237333853dbce3ba6027780110ca2f73
Yusuke Izawa <me@yizawa.com>
2025-11-11 17:19:49 +0900

    Add --jit parser

--- rpyforth/targetrpyforth.py
+++ rpyforth/targetrpyforth.py
@@ -3,14 +3,26 @@ import sys
 from rpyforth.inner_interp import InnerInterpreter
 from rpyforth.outer_interp import OuterInterpreter
 
+from rpython.rlib import jit
 from rpython.rlib.streamio import open_file_as_stream
 
 def entry_point(argv):
+    for i in range(len(argv)):
+        if argv[i] == "--jit":
+            if len(argv) == i + 1:
+                print("missing argument after --jit")
+                return 2
+            jitarg = argv[i + 1]
+            del argv[i:i+2]
+            jit.set_user_param(None, jitarg)
+            break
+
+    if len(argv) < 2:
+        print("Usage: %s filename x" % (argv[0],))
+        return 2
+
     inner = InnerInterpreter()
     outer = OuterInterpreter(inner)
-    if len(argv) < 2:
-        print "Please specify the path your target file"
-        return 1
     path = argv[1]
     f = open_file_as_stream(path)
     for line in f.readall().split('\n'):
